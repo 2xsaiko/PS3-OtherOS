f4a47cde48e2a3e36e375b56d88b1ebca209895a
Marco Rebhan <git@marco-rebhan.de>
2015-06-30 18:52:46 +0200

    Use new file splitting script

--- CFW355-OTHEROS++-SPECIAL/join.sh
+++ CFW355-OTHEROS++-SPECIAL/join.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-
-rm CFW355-OTHEROS++-SPECIAL.PUP
-echo Joining files...
-touch CFW355-OTHEROS++-SPECIAL.PUP
-cat CFW355-OTHEROS++-SPECIAL.PUP.split.0 >> CFW355-OTHEROS++-SPECIAL.PUP
-cat CFW355-OTHEROS++-SPECIAL.PUP.split.1 >> CFW355-OTHEROS++-SPECIAL.PUP
-cat CFW355-OTHEROS++-SPECIAL.PUP.split.2 >> CFW355-OTHEROS++-SPECIAL.PUP
-cat CFW355-OTHEROS++-SPECIAL.PUP.split.3 >> CFW355-OTHEROS++-SPECIAL.PUP
-echo "Done. Checking checksum (rofl)"
-if [ "$(cat CFW355-OTHEROS++-SPECIAL.PUP.md5)" = "$(md5sum CFW355-OTHEROS++-SPECIAL.PUP)" ]; then
-  echo "Joined files successfully. Enjoy your CFW355-OTHEROS++-SPECIAL.PUP!"
+CHECKSUM=e46f9cdc9b471b7bca6ced201d6f49cb
+echo "Joining files..."
+rm "./CFW355-OTHEROS++-SPECIAL.PUP" 2> /dev/null
+touch "./CFW355-OTHEROS++-SPECIAL.PUP"
+cat GW2z4GdIarx3 >> "./CFW355-OTHEROS++-SPECIAL.PUP"
+cat yyB4ipICGO4q >> "./CFW355-OTHEROS++-SPECIAL.PUP"
+cat Pf4cPFKs9D2j >> "./CFW355-OTHEROS++-SPECIAL.PUP"
+cat k96ao1Sn4Tuw >> "./CFW355-OTHEROS++-SPECIAL.PUP"
+NEWCHKSUM="$(md5sum "CFW355-OTHEROS++-SPECIAL.PUP" | cut -d ' ' -f 1)"
+if [ "x$CHECKSUM" = "x$NEWCHKSUM" ]; then
+  echo "File integrity test: PASSED"
 else
-  echo "Files not joined correctly: Expected MD5:"
-  cat CFW355-OTHEROS++-SPECIAL.PUP.md5
-  echo "Got:"
-  md5sum CFW355-OTHEROS++-SPECIAL.PUP
+  echo "[31m"
+  echo "File integrity test: FAILED"
+  echo "Expected checksum: $CHECKSUM"
+  echo "Got checksum: $NEWCHKSUM"
+  echo "Please check if any files are corrupt or missing."
+  echo "(B[m"
 fi
+echo Done!
