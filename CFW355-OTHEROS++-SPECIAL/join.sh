a2c252c5dbf1b6e6d3cc620c52c2bf8690d96951
Marco Rebhan <git@marco-rebhan.de>
2015-06-30 18:57:46 +0200

    Resplit to make sure it is 100% working

--- CFW355-OTHEROS++-SPECIAL/join.sh
+++ CFW355-OTHEROS++-SPECIAL/join.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # This file has been automatically generated. Please do not change any part of this file to ensure it is functioning correctly.
-
 CHECKSUM=e46f9cdc9b471b7bca6ced201d6f49cb
 echo "Joining files..."
 rm "./CFW355-OTHEROS++-SPECIAL.PUP" 2> /dev/null
 touch "./CFW355-OTHEROS++-SPECIAL.PUP"
-cat GW2z4GdIarx3 >> "./CFW355-OTHEROS++-SPECIAL.PUP"
-cat yyB4ipICGO4q >> "./CFW355-OTHEROS++-SPECIAL.PUP"
-cat Pf4cPFKs9D2j >> "./CFW355-OTHEROS++-SPECIAL.PUP"
-cat k96ao1Sn4Tuw >> "./CFW355-OTHEROS++-SPECIAL.PUP"
+cat 7sguaZqKC5QA >> "./CFW355-OTHEROS++-SPECIAL.PUP"
+cat Nrcri5QqtkoU >> "./CFW355-OTHEROS++-SPECIAL.PUP"
+cat xZL6vxdVm6Wr >> "./CFW355-OTHEROS++-SPECIAL.PUP"
+cat d6o16QOCdW9a >> "./CFW355-OTHEROS++-SPECIAL.PUP"
 NEWCHKSUM="$(md5sum "CFW355-OTHEROS++-SPECIAL.PUP" | cut -d ' ' -f 1)"
 if [ "x$CHECKSUM" = "x$NEWCHKSUM" ]; then
   echo "File integrity test: PASSED"
